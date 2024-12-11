f0b724624179e33abd7eafc60df627697232c25a
apsamade <bouderga.abdessamade@gmail.com>
2024-12-11 12:43:04 +0100

    fix background gradient header home page

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <header className='min-h-[90vh] bg-[url(/backgrounds/mecque-3.png)] bg-cover bg-top relative'>
-      <div className='absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-tl to-[#00000083] from-transparent'></div>
-      <div className='max-w-[1200px] m-auto'>
-        <span className='text-white text-center'>Votre chemin pour l'avenir</span>
+    <header className='min-h-[90vh] bg-[url(/backgrounds/mecque-3.png)] bg-cover bg-top relative -z-10'>
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-tl to-[#00000083] from-transparent -z-10'></div>
+      <div className='max-w-[1200px] m-auto py-12'>
+        <span className='text-white text-center block'>Votre chemin pour l'avenir</span>
         <h1 className='text-white text-5xl'></h1>
       </div>
     </header>
