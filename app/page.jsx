bc0fa6a42f75e07ba360d74cffdf9109835044d9
ScandereT78 <scanderetejpro@gmail.com>
2024-12-11 12:33:02 +0100

    commence le front

--- app/page.jsx
+++ app/page.jsx
@@ -5,8 +5,9 @@ const Home = () => {
     <>
     <header className='min-h-[90vh] bg-[url(/backgrounds/mecque-3.png)] bg-cover bg-top relative'>
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-gradient-to-tl to-[#00000083] from-transparent'></div>
-      <div>
-        <h1>Hello world</h1>
+      <div className='max-w-[1200px] m-auto'>
+        <span className='text-white text-center'>Votre chemin pour l'avenir</span>
+        <h1 className='text-white text-5xl'></h1>
       </div>
     </header>
     <main className='min-h-[300px] flex flex-wrap justify-center gap-[1px]'>
